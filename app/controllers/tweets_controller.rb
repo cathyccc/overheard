952dce2fad85f0836d2ea672cc24c4fa956bf0f1
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-02-15 21:44:38 -0500

    added twitter api; have temporary tweets show on main

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,14 @@
 class TweetsController < ApplicationController
+  before_action :get_tweets
+  def index
+    @tweets = @client.search("#overheard").take(2)
+  end
+
+  private
+  def get_tweets
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.secrets.twitter_consumer_key
+      config.consumer_secret     = Rails.application.secrets.twitter_consumer_secret
+    end
+  end
 end
