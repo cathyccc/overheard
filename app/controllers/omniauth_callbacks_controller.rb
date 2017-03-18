a92b6933cde07d607d23e1e7e71d283b5fa5121f
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2017-03-18 18:01:50 -0400

    removed unused method in user model; updated callback method to save access token when authenticating

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
      omniauth_data = (request.env["omniauth.auth"])
 
      if !UserAuthentication.where(user_id: current_user.id, authentication_provider: omniauth_data.provider).persisted?
-       @authentication = @user.user_authentications.build(authentication_provider: omniauth_data.provider, uid: omniauth_data.uid)
+       @authentication = @user.user_authentications.build(authentication_provider: omniauth_data.provider, uid: omniauth_data.uid, access_token: omniauth_data.credentials.token, access_secret: omniauth_data.credentials.secret)
        @authentication.save
      end
      redirect_to profile_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,4 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:twitter]
   has_many :tracked_terms, inverse_of: :user
   has_many :user_authentications, inverse_of: :user
-
-  # def self.from_omniauth(auth)
-  #   user.user_authentications.provider = auth.provider
-  #   user.user_authentications.uid = auth.uid
-  # end
 end
